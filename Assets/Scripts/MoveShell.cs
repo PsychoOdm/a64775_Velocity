9893ee1e25730a220dfaa62f504ba6e979fc0ae0
PsychoOdm <escolamarianacosta124@gmail.com>
2026-09-22 23:41:38 +0100

    1_Velocity

---
+++ Assets/Scripts/MoveShell.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MoveSell : MonoBehaviour
+{
+    float speed = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.Translate(0, 0, Time.deltaTime * speed);
+        this.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
+    }
+}
