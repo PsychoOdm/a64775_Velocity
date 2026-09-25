b8972b14c868f386bc4561f5ed7587ed9569226a
PsychoOdm <escolamarianacosta124@gmail.com>
2026-09-26 00:12:35 +0100

    2_SolvingParabolic_Arc_to_Find_the_HitLocation

--- Assets/Scripts/FileShell.cs
+++ Assets/Scripts/FileShell.cs
@@ -5,7 +5,9 @@ public class FireShell : MonoBehaviour
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
+    float  speed = 15;
     float rotSpeed = 2;
+    public Transform turretBase;
 
     void CreateBullet()
     {
@@ -16,6 +18,40 @@ public class FireShell : MonoBehaviour
         );
     }
 
+    void RotateTurret()
+    {
+    float? angle = CalculateAngle(true);
+    if(angle !=null)
+    {
+        turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+    }
+    }
+
+
+    float? CalculateAngle(bool low)
+    {
+       Vector3 targetDir = enemy.transform.position - this.transform.position;
+       float y = targetDir.y;
+       targetDir.y = 0f;
+       float x = targetDir.magnitude;
+       float gravity = 9.8f;
+       float sSqr = speed * speed;
+       float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+       
+       if (underTheSqrRoot >= 0f)
+       {
+        float root = Mathf.Sqrt(underTheSqrRoot);
+        float highAngle = sSqr + root;
+        float lowAngle = sSqr - root;
+
+        if(low)
+            return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+        else 
+            return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+       }
+    else
+        return null;
+    }
  
 
     void Update()
@@ -23,6 +59,9 @@ public class FireShell : MonoBehaviour
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation,lookRotation, Time.deltaTime * rotSpeed);
+        
+        RotateTurret();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
           CreateBullet();
