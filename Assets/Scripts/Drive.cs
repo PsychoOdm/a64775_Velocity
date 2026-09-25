cd0a33f07b853d4345ff0479a5cd195401b86672
PsychoOdm <escolamarianacosta124@gmail.com>
2026-09-25 21:57:31 +0100

    1_IntroducingAcceleration

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -6,6 +6,9 @@ public class Drive : MonoBehaviour
 {
     public float speed = 1.0f;
     public float rotationSpeed = 100.0f;
+    public Transform transGun;
+    public Transform gun;
+    public GameObject bulletObj;
 
     void Update()
     {
@@ -20,10 +23,23 @@ public class Drive : MonoBehaviour
         rotation *= Time.deltaTime;
 
         // Move translation along the object's z-axis
-        //transform.Translate(0, 0, translation);
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Translate(0, 0, translation);
+        //transform.Translate(0, 0, speed * Time.deltaTime);
 
         // Rotate around our y-axis
-        //transform.Rotate(0, rotation, 0);
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            transGun.RotateAround(transGun.position, transGun.right, -2);
+        }
+        else if(Input.GetKey (KeyCode.G))
+        {
+            transGun.RotateAround(transGun.position, transGun.right, 2);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(bulletObj, gun.position, gun.rotation);
+        }
     }
 }
